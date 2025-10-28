6ad3c4e9371457ba5bf255990f8768add226a1a8
BhaskarAditya <bhaskaraditya0712@gmail.com>
2025-10-28 11:57:09 +0000

    commit message

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,16 @@
 import {Component} from 'react'
-
 import Loader from 'react-loader-spinner'
 
 import TeamCard from '../TeamCard'
 
 import './index.css'
 
+const teamsApiUrl = 'https://apis.ccbp.in/ipl'
+
 class Home extends Component {
   state = {
     isLoading: true,
-    teamsList: [],
+    teamsData: [],
   }
 
   componentDidMount() {
@@ -17,35 +18,43 @@ class Home extends Component {
   }
 
   getTeams = async () => {
-    const response = await fetch('https://apis.ccbp.in/ipl')
-    const data = await response.json()
-    const updatedData = data.teams.map(eachTeam => ({
-      id: eachTeam.id,
-      name: eachTeam.name,
-      teamImageURL: eachTeam.team_image_url,
+    const response = await fetch(teamsApiUrl)
+    const fetchedData = await response.json()
+    const formattedData = fetchedData.teams.map(team => ({
+      name: team.name,
+      id: team.id,
+      teamImageURL: team.team_image_url,
     }))
-    this.setState({teamsList: updatedData, isLoading: false})
+
+    this.setState({
+      teamsData: formattedData,
+      isLoading: false,
+    })
   }
 
-  renderingTeams = () => {
-    const {teamsList} = this.state
+  renderTeamsList = () => {
+    const {teamsData} = this.state
+
     return (
       <ul className="teams-list">
-        {teamsList.map(eachTeam => (
-          <TeamCard teamData={eachTeam} key={eachTeam.id} />
+        {/* FIX6: The list of team cards should be rendered using Array.map() method */}
+        {teamsData.map(team => (
+          <TeamCard teamDetails={team} key={team.id} />
         ))}
       </ul>
     )
   }
 
-  renderingLoading = () => (
-    <div data-testid="loader" className="loader-container">
-      <Loader type="Oval" color="#ffffff" height="50" />
+  renderLoader = () => (
+    // FIX7: For the purpose of testing here testid attribute should be added with the value "loader"
+    <div testid="loader" className="loader-container">
+      <Loader type="Oval" color="#ffffff" height={50} />
     </div>
   )
 
   render() {
     const {isLoading} = this.state
+
     return (
       <div className="home-route-container">
         <div className="teams-list-container">
@@ -57,7 +66,7 @@ class Home extends Component {
             />
             <h1 className="ipl-dashboard-heading">IPL Dashboard</h1>
           </div>
-          {isLoading ? this.renderingLoading() : this.renderingTeams()}
+          {isLoading ? this.renderLoader() : this.renderTeamsList()}
         </div>
       </div>
     )
